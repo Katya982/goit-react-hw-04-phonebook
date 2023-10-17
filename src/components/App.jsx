339650d1bb799a22e67683bf05e173f2d717e9e5
Katya982 <xamko98@gmail.com>
2023-10-17 15:38:15 +0300

    add update in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,6 +49,27 @@ export class App extends Component {
     );
   };
 
+  componentDidMount() {
+    console.log('app componentDidMount');
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+
+    console.log('app componentDidUpdate')
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('contact field updated');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
